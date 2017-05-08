89eabe7eb4bef700b60ebfd8cf2b8cad51038eb4
Miles Aylward <miles.aylward@gmail.com>
2017-05-07 22:37:51 -0400

    test

--- index.js
+++ index.js
@@ -2,16 +2,6 @@ const app = require('./server/server');
 
 app.set('port', (process.env.PORT || 5000));
 
-app.use(express.static(__dirname + '/public'));
-
-// views is directory for all template files
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
-
-app.get('/', function(request, response) {
-  response.render('pages/index');
-});
-
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
