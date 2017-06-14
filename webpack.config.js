518450d836cadd67261e419fa64428c50883571a
milesaylward <miles.aylward@gmail.com>
2017-06-14 15:28:22 -0400

    w

--- webpack.config.js
+++ webpack.config.js
@@ -27,7 +27,7 @@ module.exports = {
             loader: 'file-loader',
             options: {
               query: {
-                name:'assets/[name].[ext]'
+                name:'/[name].[ext]'
               }
             }
           },
